7cc4bf24152d03b930f75e6ee658c90134743e52
acer linux <luanbianchine1@gmail.com>
2022-05-01 13:33:16 -0300

    applying environment variables

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,19 +1,18 @@
-import firebase from 'firebase/app';
+import {initializeApp} from 'firebase/app';
 
 import {getAuth} from 'firebase/auth';
 import {getDatabase} from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC9WdL66Ge4JKbSzNDHxX9sK1hS9bIeArU",
-  authDomain: "letmeask-19edd.firebaseapp.com",
-  databaseURL: "https://letmeask-19edd-default-rtdb.firebaseio.com",
-  projectId: "letmeask-19edd",
-  storageBucket: "letmeask-19edd.appspot.com",
-  messagingSenderId: "827487382312",
-  appId: "1:827487382312:web:078438ec90bec4b862be1d"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
-firebase.initializeApp(firebaseConfig);
-
+export const firebase = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export const database = getDatabase();
