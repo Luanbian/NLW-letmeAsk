8c7022c1e4d3269475960f96dd162715b5b06be0
acer linux <luanbianchine1@gmail.com>
2022-05-01 22:19:04 -0300

    newroom page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { Home } from "./pages/Home";
+//import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
 import './styles/global.scss';
 
 function App() {
   return (
     <>
-      <Home/>
+      <NewRoom/>
     </>
  );
 }
